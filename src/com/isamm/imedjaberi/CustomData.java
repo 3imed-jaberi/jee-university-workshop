f3bf6c5e5eb72113068b8ae1aa606a98715b25a8
unknown <imed_jebari@hotmail.fr>
2019-05-14 21:25:43 +0200

    all global dara ( const ) like url for connection ..

---
+++ src/com/isamm/imedjaberi/CustomData.java
@@ -0,0 +1,8 @@
+package com.isamm.imedjaberi;
+
+public final class CustomData {
+
+	  
+	  public static final String URL = "jdbc:mysql://localhost:3306/__DB_NAME__?user=__USER__&password=__PASSWORD__&useSSL=false&useTimezone=true&serverTimezone=UTC";
+
+}
