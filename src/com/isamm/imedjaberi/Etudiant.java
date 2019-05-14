2076cda785b047f49081fce1884a7f1556c232b6
unknown <imed_jebari@hotmail.fr>
2019-05-14 21:28:39 +0200

    this class represent the 'etudiant' table in the DB ..

---
+++ src/com/isamm/imedjaberi/Etudiant.java
@@ -0,0 +1,43 @@
+package com.isamm.imedjaberi;
+
+public class Etudiant {
+	private int Id ; 
+	private String Nom , Prenom , Tel , Niveau ;
+	
+	// Here all the getters & setters for the Etudiant object .. 
+	
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public String getNom() {
+		return Nom;
+	}
+	public void setNom(String nom) {
+		Nom = nom;
+	}
+	public String getPrenom() {
+		return Prenom;
+	}
+	public void setPrenom(String prenom) {
+		Prenom = prenom;
+	}
+	public String getTel() {
+		return Tel;
+	}
+	public void setTel(String tel) {
+		Tel = tel;
+	}
+	public String getNiveau() {
+		return Niveau;
+	}
+	public void setNiveau(String niveau) {
+		Niveau = niveau;
+	}
+	
+	
+	
+
+}
